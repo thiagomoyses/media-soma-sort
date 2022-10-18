5d6e7ff7827255718caeedfd1d8708cee6779bb6
THIAGO MOYSES <thiagodasilvamoysesfr@gmail.com>
2022-10-18 16:10:25 +0200

    Creating first design for the cards

---
+++ src/components/Card.jsx
@@ -0,0 +1,23 @@
+import './card.css';
+import React from 'react';
+
+function getColor(props){
+    if(props.red) return "Red"
+    if(props.green) return "Green"
+    if(props.blue) return "Blue"
+    if(props.purple) return "Purple"
+}
+
+export default function Card(props){
+    return(
+        <div className={`card ${getColor(props)}`}>
+            <div className="header">
+                <span className='title'>{ props.title }</span>
+            </div>
+
+            <div className="content">
+                { props.children }
+            </div>
+        </div>
+    );
+}
